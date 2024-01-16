682dcf74458c7eb7d660d5c83a60ccb8b0194711
Ebrahem Luhar <eb.luhar@gmail.com>
2024-01-16 21:26:44 +0000

    Add items to list functionality

---
+++ app.js
@@ -0,0 +1,21 @@
+const searchForm = document.querySelector('.js-search-form');
+const searchInput = document.querySelector('.js-search-input');
+const listParent = document.querySelector('.js-list-group');
+const addItemForm = document.querySelector('.js-add-form');
+const addItemInput = document.querySelector('.js-add-input');
+
+function addNewTask(newItem) {
+  const newListItem = `
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span class="list-group-text">${newItem}</span>
+    <i class="list-group-icon far fa-trash-alt delete"></i>
+  </li>
+  `;
+  listParent.innerHTML += newListItem;
+}
+
+addItemForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newItem = addItemInput.value.trim();
+  addNewTask(newItem);
+});
