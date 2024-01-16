0914142669b00ba6f89f86d5bdc995fa668507a7
Ebrahem Luhar <eb.luhar@gmail.com>
2024-01-16 21:35:23 +0000

    Delete task

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const listParent = document.querySelector('.js-list-group');
 const addItemForm = document.querySelector('.js-add-form');
 const addItemInput = document.querySelector('.js-add-input');
 
+/**
+ * Add new task
+ */
 function addNewTask(newItem) {
   const newListItem = `
   <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -19,3 +22,13 @@ addItemForm.addEventListener('submit', (e) => {
   const newItem = addItemInput.value.trim();
   addNewTask(newItem);
 });
+
+/**
+ * Delete task
+ */
+listParent.addEventListener('click', (e) => {
+  console.log(e.target);
+  if (e.target.classList.contains('js-delete')) {
+    e.target.parentElement.remove();
+  }
+})
